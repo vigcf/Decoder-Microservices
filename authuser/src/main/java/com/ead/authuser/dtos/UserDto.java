7540baa73a1327f1a2619a89830507f466d9355b
Vitória <vitoriagcf@hotmail.com>
2022-08-21 16:51:48 -0300

    Validações com Spring Validation
    Mapeando restrições no UserDTO
    Mostrando cada restrição nos metódos do controller

--- authuser/src/main/java/com/ead/authuser/dtos/UserDto.java
+++ authuser/src/main/java/com/ead/authuser/dtos/UserDto.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 @Data
@@ -25,21 +28,39 @@ public class UserDto {
     }
 
     private UUID userId;
+
+    @Size(min = 4, max = 50)
     @JsonView(UserView.RegistrationPost.class)
+    @NotBlank(groups = UserView.RegistrationPost.class)
     private String username;
+
+    @Email
+    @Size(min = 4, max = 50)
     @JsonView(UserView.RegistrationPost.class)
+    @NotBlank(groups = UserView.RegistrationPost.class)
     private String email;
+
+    @Size(min = 6, max = 20)
     @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
+    @NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
-    @JsonView({UserView.PasswordPut.class})
+
+    @Size(min = 6, max = 20)
+    @JsonView(UserView.PasswordPut.class)
+    @NotBlank(groups = UserView.PasswordPut.class)
     private String oldPassword;
+
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String fullName;
+
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String phoneNumber;
+
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String cpf;
+
     @JsonView({UserView.ImagePut.class})
+    @NotBlank(groups = UserView.ImagePut.class)
     private String imageUrl;
 
 }
